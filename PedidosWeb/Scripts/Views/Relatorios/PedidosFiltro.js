80866a70ca45905af757e773433b0cc37c9d87d4
thomasvier <thomasvier@gmail.com>
2016-03-02 09:11:12 -0300

    rel pedido

---
+++ PedidosWeb/Scripts/Views/Relatorios/PedidosFiltro.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function()
+{
+    $('#btnGerarRelatorio').click(function () {
+        var url = '';
+        var parametros = false;
+
+        if ($('#ddlClientes').val() != null)
+        {
+            url = 'idCliente=' + $('#ddlClientes').val();
+            parametros = true;
+        }
+
+        if ($('#DataInicial').val() != null)
+        {
+            if (url != null) {
+                url += '&dataInicial=' + $('#DataInicial').val();
+            }
+            else
+            {
+                url += 'dataInicial=' + $('#DataInicial').val();
+            }
+
+            parametros = true;
+        }
+
+        if($('#DataFinal').val() != null)
+        {
+            if (url != null) {
+                url += '&dataFinal=' + $('#DataFinal').val();
+            }
+            else {
+                url += 'dataFinal=' + $('#DataFinal').val();
+            }
+                
+            parametros = true;
+        }
+
+        if (parametros) {
+            window.open('/Relatorios/Pedidos?' + url);
+        }
+        else {
+            window.open('/Relatorios/Pedidos');
+        }
+    });
+})
